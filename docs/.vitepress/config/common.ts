b33338421fc330a74445b681e7c4419f713f3cc8
KeyMatter <cheney233@outlook.com>
2023-07-25 14:27:42 +0800

    ccc

--- docs/.vitepress/config/common.ts
+++ docs/.vitepress/config/common.ts
@@ -1,2 +1,2 @@
-export const BASE_URL: string = '/vite-press/' as const
-export const withBase = (path: string): string => `${ BASE_URL + path }`.replace(/\/+/g, '/')
+export const BASE_URL: string = '/blog/' as const
+export const withBase = (path: string): string => `${BASE_URL + path}`.replace(/\/+/g, '/')
